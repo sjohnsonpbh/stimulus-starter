33744b406ebf6ce15a588ac980d630f5d247e8ca
Scott Johnson <47424009+sjohnsonpbh@users.noreply.github.com>
2023-08-09 09:55:54 -0500

    stimulus - exercises 1-3

---
+++ src/controllers/clipboard_controller.js
@@ -0,0 +1,11 @@
+// src/controllers/clipboard_controller.js
+
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["source"]
+  copy(event) {
+    event.preventDefault()
+    navigator.clipboard.writeText(this.sourceTarget.value)
+  }
+}
---
+++ src/controllers/hello_controller.js
@@ -0,0 +1,11 @@
+// src/controllers/hello_controller.js
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["name"]
+  greet() {
+    const element = this.nameTarget
+    const name = element.value
+    console.log(`Hello, ${name}!`)
+  }
+}
