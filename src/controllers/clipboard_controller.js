d85aca203380ef36567a1d7d5589e1d13f1cbb8a
Scott Johnson <47424009+sjohnsonpbh@users.noreply.github.com>
2023-08-10 11:46:38 -0500

    exercises 4-5

--- src/controllers/clipboard_controller.js
+++ src/controllers/clipboard_controller.js
@@ -4,8 +4,15 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   static targets = ["source"]
+  static classes = ["supported"]
   copy(event) {
     event.preventDefault()
     navigator.clipboard.writeText(this.sourceTarget.value)
+  }
+
+  connect() {
+    if ("clipboard" in navigator) {
+      this.element.classList.add(this.supportedClass);
+    }
   }
 }
